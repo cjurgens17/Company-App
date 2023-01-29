2192be60fbee485f2fadff43f696072295d234cd
chris <104324755+cjurgens17@users.noreply.github.com>
2023-01-29 16:11:41 -0500

    Add/Update Employees

--- src/main/java/com/personPractice/controllers/EmployeeController.java
+++ src/main/java/com/personPractice/controllers/EmployeeController.java
@@ -2,14 +2,18 @@ package com.personPractice.controllers;
 
 
 import com.personPractice.models.Employee;
-import com.personPractice.repository.EmployeeRepository;
 import com.personPractice.services.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Set;
+
+
 
 @Controller
 @RequestMapping({"employees"})
@@ -33,5 +37,47 @@ public class EmployeeController {
         model.addAttribute("employee",employeeService.findById(Long.valueOf(id)));
 
         return "employee/show";
+    }
+
+    @GetMapping("/addEmployee")
+    public String employeeForm(Employee employee){
+        return "employee/addEmployee";
+    }
+
+    @PostMapping({"/addEmployee"})
+    public String addEmployee(@Valid Employee employee, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "employee/addEmployee";
+        }
+      //  model.addAttribute("employees", employeeService.findAll());
+        employeeService.save(employee);
+        return "redirect:/employees/index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable Long id, Model model){
+        Employee employee = employeeService.findById(id);
+
+        model.addAttribute("employee", employee);
+        return "employee/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateEmployee(@PathVariable Long id, @Valid Employee employee, BindingResult result, Model model){
+        if(result.hasErrors()){
+            employee.setId(id);
+            return "employee/update";
+        }
+       // model.addAttribute("employees",employeeService.getEmployees());
+        employeeService.save(employee);
+        return "redirect:/employees/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id, Model model){
+        Employee employee = employeeService.findById(id);
+        employeeService.delete(employee);
+      //  model.addAttribute("employees",employeeService.getEmployees());
+        return "redirect:/employees/index";
     }
 }
