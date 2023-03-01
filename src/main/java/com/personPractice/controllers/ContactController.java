e1ab0b9009b2b9cb885e8ba7175261bd74f62703
chris <104324755+cjurgens17@users.noreply.github.com>
2023-03-01 14:48:11 -0500

    Got contact form working properly

--- src/main/java/com/personPractice/controllers/ContactController.java
+++ src/main/java/com/personPractice/controllers/ContactController.java
@@ -3,6 +3,8 @@ package com.personPractice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +16,6 @@ public class ContactController {
     public String getContact(){
         return "contact";
     }
+
+
 }
